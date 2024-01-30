a8d6a18d879b1b9abe1a863b95e4208c5d46626d
Ponsin Hugo <ponsinh@etu.univ-grenoble-alpes.fr>
2024-01-30 20:52:23 +0100

    passage sur l'ordinateur personelle

---
+++ contour.c
@@ -0,0 +1,30 @@
+#include "contour.h"
+#include "types_macros.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+Tableau GetOutline(unsigned int stratingx, unsigned int startingy, Image * img)
+{
+    NuttyNoodler nuttnutt;
+    nuttnutt.orientation = Est;
+    nuttnutt.x = stratingx;
+    nuttnutt.y = startingy;
+    do
+    {
+        
+    } while (/* condition */);
+    
+    
+}
+
+Pixel GetNuttNuttLeftPixelValue(NuttyNoodler);
+
+NuttyNoodler * InitNuttyNoodler(unsigned int stratingx, unsigned int stratingy)
+{
+    NuttyNoodler * nuttnutt = (NuttyNoodler *)malloc(sizeof(NuttyNoodler));
+    nuttnutt->x = stratingx;
+    nuttnutt->y = stratingy;
+    nuttnutt->orientation;
+    return nuttnutt;
+}
